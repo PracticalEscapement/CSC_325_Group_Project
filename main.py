5e4924a62618bc9c21fadad15954ce90dea84cf5
Oscar Dejesus <oscar6dejesus@gmail.com>
2025-03-07 13:21:11 -0500

    added db and imported flask login

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import signal
 import sys
 from website import create_app
-
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = create_app()
+db =SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///database.db'
+app.config['SECRET_KEY']='thisisakey'
 
 # comment
 
