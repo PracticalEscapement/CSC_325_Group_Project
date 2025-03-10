476e357a2264a02ead3223c7d6ff839347b7ef3c
Oscar Dejesus <oscar6dejesus@gmail.com>
2025-03-09 23:14:00 -0400

    login and creating database for user

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import signal
 import sys
 from website import create_app
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+
 
 app = create_app()
-db =SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///database.db'
-app.config['SECRET_KEY']='thisisakey'
-
 
 
 if __name__ == '__main__':
-    app.run(debug=True)     #set it to False when run it in prodaction
+    app.run(debug=True)     #set ixt to False when run it in prodaction
