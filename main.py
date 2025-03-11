d73d278ccc1b0a6bb6698dc99dbc8518dc81fc80
Oscar-Dejesus <oscar6dejesus@gmail.com>
2025-03-11 18:25:48 -0400

    main.py change

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
 app = create_app()
-
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///database.db'
 app.config['SECRET_KEY']='thisisakey'
 db =SQLAlchemy(app)     #set it to False when run it in prodaction
