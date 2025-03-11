b9fc26f1fc8d197bce52caf18731535069c28c82
Oscar-Dejesus <oscar6dejesus@gmail.com>
2025-03-11 18:24:27 -0400

    main.py change

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import signal
 import sys
 from website import create_app
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = create_app()
 
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///database.db'
+app.config['SECRET_KEY']='thisisakey'
+db =SQLAlchemy(app)     #set it to False when run it in prodaction
 
+
+
+ 
 if __name__ == '__main__':
-    app.run(debug=True)     #set it to False when run it in prodaction
+     app.run(debug=True)     #set it to False when run it in prodaction
