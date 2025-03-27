14c2fcb665a284c8698f968282f57b5311942fa9
Oscar-Dejesus <oscar6dejesus@gmail.com>
2025-03-26 21:55:04 -0400

    added passwords hases and increments id when new user is added

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template,request,flash,redirect,url_for
 from . import db
 from .models import user
 from flask_login import login_user,logout_user,login_required,current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods =['GET','POST'])
@@ -14,9 +15,9 @@ def login():
 
         if email_exists:
             
-            if email_exists.password ==password:
+            if check_password_hash(email_exists.password,password):
                 login_user(email_exists,remember=True)
-                flash("logged in", category='success')
+                flash(f"logged in you id is: +{email_exists.id}", category='success')
                 return redirect(url_for('views.home'))
             else:
                 flash("Wrong password",category='error')
@@ -47,7 +48,7 @@ def sign_up():
         elif password1!=password2:
             flash("Passwords dont match.",  category = 'error')
         else:
-            new_User= user(id =12345,email = email,username =Fname +" " +Lname, password=password1)
+            new_User= user(email = email,username =Fname +" " +Lname, password=generate_password_hash(password1))
             db.session.add(new_User)
             db.session.commit()
             
--- website/models.py
+++ website/models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 
 
 class user(db.Model,UserMixin):
-    id = db.Column(db.String(150),primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True,nullable=False) 
     email= db.Column(db.String(150), nullable=False)
     username= db.Column(db.String(150), nullable=False)
     password = db.Column(db.String(150), nullable=False)
