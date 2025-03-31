e65a39b851baeb5ee2ce030125bdebb90de1bc5a
Oscar-Dejesus <oscar6dejesus@gmail.com>
2025-03-31 19:33:56 -0400

    added sessions to log in and out

--- website/auth.py
+++ website/auth.py
@@ -1,20 +1,26 @@
-from flask import Blueprint, render_template,request,flash,redirect,url_for
+from flask import Blueprint, render_template,request,flash,redirect,url_for,session
 from . import db
 from .models import user
 from flask_login import login_user,logout_user,login_required,current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta
 auth = Blueprint('auth', __name__)
-
+auth.permanent_session_lifetime=timedelta(days=365*10)
 @auth.route('/login', methods =['GET','POST'])
 def login():
+    if"user" in session:
+        flash("Already Logged in","info")
+        return redirect(url_for('views.home'))
     if request.method=='POST':
+        session.permanent=True
         email = request.form.get('email')
+        
         password = request.form.get('password')
         email_exists= user.query.filter_by(email=email).first()
 
 
         if email_exists:
-            
+            session["user"]=email
             if check_password_hash(email_exists.password,password):
                 login_user(email_exists,remember=True)
                 flash(f"logged in you id is: +{email_exists.id}", category='success')
@@ -28,8 +34,14 @@ def login():
 
 @auth.route('/logout')
 def logout():
-    logout_user()
-    return "<p>Logout</p>"
+    if "user" in session:
+        user = session["user"]
+        
+        flash(f"You have been logged out, {user}")
+    else:
+        flash("You are already logged out")
+    session.pop("user",None)
+    return redirect(url_for('views.home'))
 
 @auth.route('/sign-up', methods=['POST','GET'])
 def sign_up():
@@ -56,3 +68,5 @@ def sign_up():
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
+
+    
