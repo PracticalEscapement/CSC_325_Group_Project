f5130043216e340aa20e05f9fbbb13236654b2a6
colaola20 <soryo@farmingdale.edu>
2025-04-10 21:29:36 -0400

    added some validation to sign up form

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
     app.config['SECRET_KEY'] = 'ok30vXjg5n'
 
     from .views import views
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,44 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        pass
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        #data = request.form
+        #print(data)
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # need to do some data validation
+        # for now I'm doing some simple ones, but it needs to be improved in the future (possibly using regex expressions)
+        if len(email) < 4:
+            flash('Email must be at least 4 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be at least 2 characters.', category='error')
+        elif len(last_name) < 2:
+            flash('Last name must be at least 2 characters.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        elif password1 != password2:
+            flash('The passwords don\'t match.', category='error')
+        else:
+            flash('Account created!', categoty='success')
+            # add user to the database
+
+
+
     return render_template("sign_up.html")
