273a9bbaa9a270a41cf29db239be85b7696e7694
Oscar-Dejesus <oscar6dejesus@gmail.com>
2025-04-29 18:03:39 -0400

    Secret Key put in env file

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import json
 from website import create_app
 from website.models import User
 from website.models import *
+import os
 
 
 app = create_app()
-app.config['SECRET_KEY']='6b6466ab36b643cd8185b7e283ac5007'
+app.config['SECRET_KEY'] = os.environ.get(SECRET_KEY)
 if __name__ == '__main__':
     #with app.app_context():
        # response = get_user(1).get_json()
