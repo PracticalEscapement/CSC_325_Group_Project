9576f07df63c979960e4d4c09b653188e36ceecd
Oscar-Dejesus <oscar6dejesus@gmail.com>
2025-04-29 11:59:04 -0400

    fixed issue in views.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from website.models import *
 app = create_app()
 app.config['SECRET_KEY']='6b6466ab36b643cd8185b7e283ac5007'
 if __name__ == '__main__':
-    with app.app_context():
-        response = get_user(1).get_json()
+    #with app.app_context():
+       # response = get_user(1).get_json()
     app.run(debug=True)
 
     
